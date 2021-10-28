bfe6a090d7449e6f14a7ee817bd387e41b01d8ec
kumarmukeshpatel57 <kumarmukeshpatel57@gmail.com>
2021-10-28 12:32:31 +0530

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_app_flutter/pages/home_page.dart';
-import 'package:todo_app_flutter/provider/todos.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'Screens/home_page.dart';
+
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  static final String title = 'Todo App';
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) => ChangeNotifierProvider(
-    create: (context) => TodosProvider(),
-    child: MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: title,
       theme: ThemeData(
-        primarySwatch: Colors.pink,
-        scaffoldBackgroundColor: Color(0xFFf6f5ee),
+        textTheme: GoogleFonts.nunitoSansTextTheme(
+          Theme.of(context).textTheme,
+        ),
       ),
       home: HomePage(),
-    ),
-  );
+    );
+  }
 }
